116e74ed62b04905011aaa66314fbf5df37f58a0
OLEGATOR1337 <94855097+OLEGATOR1337@users.noreply.github.com>
2021-12-04 01:46:21 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,29 @@
+import pylab
+import numpy as np
+import json
+import pathlib
+import math
+
+def f(x):
+    return  (np.sin(3 * math.pi * x) ** 3) + ((x - 1)**2) * (1 + (np.sin (math.pi * 3))**2)
+
+x_min = -10
+x_max = 10
+dx = 0.001
+
+x = np.arange(x_min, x_max, dx)
+y = f(x)
+
+res = {
+"data": [
+{"x": float(x1), "y": float(y1)} for x1, y1 in zip(x, y)
+]
+}
+
+path = pathlib.Path("results")
+path.mkdir(exist_ok=True)
+file = path / "result_task1.json"
+
+pylab.plot(x, y)
+pylab.grid()
+pylab.savefig("results/task1.png")
